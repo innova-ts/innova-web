f13d529bdeab229b6df69d40226e3759d0fdd0bb
Victor <victorvelas.lst@gmail.com>
2026-08-08 23:31:16 -0500

    feat: implement home page layout and interactive services grid component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { navigateTo } from '#app';
 import SmoothLiquid from '~/components/shared/ui/SmoothLiquid.vue';
+import HomeServicesGrid from '~/components/ui/home/HomeServicesGrid.vue';
 import Button from '~/components/web/forms/Button.vue';
 
 const goTo = (destiny: string) => {
@@ -31,39 +32,7 @@ const goTo = (destiny: string) => {
         <!-- Services -->
         <section class="min-h-[300px] bg-main-900/4">
             <div class="container-site py-10">
-                <div class="mb-7">
-                    <h2 class="text-2xl font-bold">{{ $t('home.services.title') }}</h2>
-                    <h3 class="text-md dark:text-tod/70">{{ $t('home.services.description') }}</h3>
-                </div>
-                <div>
-                    <div class="grid grid-cols-1 sm:grid-cols-3 gap-2">
-                        <article class="border-1 min-h-[250px] bg-white dark:bg-zinc-800/35 border-gray-200/90 dark:border-tol/70 hover:border-main-50 transition-all duration-200 p-4 sm:col-span-2 relative group/title">
-                            <div class="absolute bottom-4 mr-4">
-                                <h3 class="text-lg font-bold my-2 border-l-4 border-main-50 pl-3 group-hover/title:text-main-100 transition-all duration-200">{{ $t('home.services.mainServices.custom.title') }}</h3>
-                                <p class="text-tol/70 dark:text-tod/80">{{ $t('home.services.mainServices.custom.content') }}</p>
-                            
-                            </div>
-                        </article>
-                        <article class="border-1 min-h-[250px] bg-white dark:bg-zinc-800/35 border-gray-200/90 dark:border-tol/70 hover:border-main-50 transition-all duration-200 p-4 relative group/title">
-                            <div class="absolute bottom-4 mr-4">
-                                <h3 class="text-lg font-bold my-2 border-l-4 border-main-50 pl-3 group-hover/title:text-main-100 transition-all duration-200">{{ $t('home.services.mainServices.web.title') }}</h3>
-                                <p class="text-tol/70 dark:text-tod/80">{{ $t('home.services.mainServices.web.content') }}</p>
-                            </div>
-                        </article>
-                        <article class="border-1 min-h-[250px] bg-white dark:bg-zinc-800/35 border-gray-200/90 dark:border-tol/70 hover:border-main-50 transition-all duration-200 p-4 relative group/title">
-                            <div class="absolute bottom-4 mr-4">
-                                <h3 class="text-lg font-bold my-2 border-l-4 border-main-50 pl-3 group-hover/title:text-main-100 transition-all duration-200">{{ $t('home.services.mainServices.mobile.title') }}</h3>
-                                <p class="text-tol/70 dark:text-tod/80">{{ $t('home.services.mainServices.mobile.content') }}</p>
-                            </div>
-                        </article>
-                        <article class="border-1 min-h-[250px] bg-white dark:bg-zinc-800/35 border-gray-200/90 dark:border-tol/70 hover:border-main-50 transition-all duration-200 p-4 sm:col-span-2 relative group/title">
-                            <div class="absolute bottom-4 mr-4">
-                                <h3 class="text-lg font-bold my-2 border-l-4 border-main-50 pl-3 group-hover/title:text-main-100 transition-all duration-200">{{ $t('home.services.mainServices.infrastructure.title') }}</h3>
-                                <p class="text-tol/70 dark:text-tod/80">{{ $t('home.services.mainServices.infrastructure.content') }}</p>
-                            </div>
-                        </article>
-                    </div>
-                </div>
+                <HomeServicesGrid />
             </div>
         </section>
 
