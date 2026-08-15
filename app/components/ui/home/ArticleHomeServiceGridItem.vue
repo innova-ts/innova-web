53f20a5364b86fad03c49358fc44b51b3593a027
Victor <victorvelas.lst@gmail.com>
2026-08-15 01:59:32 -0500

    feat: implement TestimonialsList component and add localized testimonial content

--- app/components/ui/home/ArticleHomeServiceGridItem.vue
+++ app/components/ui/home/ArticleHomeServiceGridItem.vue
@@ -13,7 +13,7 @@ const props = defineProps<{
 </script>
 <template>
     <article 
-        class="rounded-md sm:rounded-lg border-1 min-h-[250px] bg-white dark:bg-zinc-800/35 border-gray-200/90 dark:border-tol/70 hover:border-main-50 transition-all duration-200 relative group/title overflow-hidden shadow-bod/25 dark:shadow-main-800/40 hover:shadow-[-1px_5px_3px] focus:shadow-[-1px_5px_3px] dark:focus:shadow-[-1px_5px_8px] hover:translate-y-[-6px] focus:translate-y-[-6px]" 
+        class="rounded-md sm:rounded-lg border-1 min-h-[250px] bg-white dark:bg-zinc-800/35 border-gray-200/90 dark:border-tol/70 hover:border-main-50 transition-all duration-200 relative group/title overflow-hidden shadow-bod/30 dark:shadow-main-800/40 hover:shadow-[-1px_5px_3px] focus:shadow-[-1px_5px_3px] dark:focus:shadow-[-1px_5px_8px] hover:translate-y-[-6px] focus:translate-y-[-6px]" 
         :class="[$attrs.class]"
         tabindex="0"
     >
@@ -28,16 +28,19 @@ const props = defineProps<{
                 v-if="props.imgUrl"
                 :src="props.imgUrl"
                 :alt="props.alt ?? ''"
-                class="w-full h-full opacity-[50%] dark:opacity-20 group-hover/title:opacity-90 group-focus/title:opacity-90 dark:group-hover/title:opacity-40 dark:group-focus/title:opacity-40 object-cover scale-105 transition-all duration-700 ease-out group-hover/title:scale-110"
+                class="w-full h-full opacity-[85%] dark:opacity-30 group-hover/title:opacity-90 group-focus/title:opacity-90 dark:group-hover/title:opacity-40 dark:group-focus/title:opacity-40 object-cover scale-105 transition-all duration-700 ease-out group-hover/title:scale-110"
                 aria-hidden="true" 
                 loading="lazy" 
                 decoding="async" 
             />
         </div>
-        <section class="absolute bottom-0 p-0 transition-all group-hover/title:backdrop-blur-[3px] group-focus/title:backdrop-blur-[3px] group-hover/title:bg-white/80 group-focus/title:bg-white/80 dark:group-hover/title:bg-bod/80 dark:group-focus/title:bg-bod/80">
-            <div class="mx-4 z-10 translate-y-[calc(100%-40px)] group-hover/title:translate-y-0 group-focus/title:translate-y-0 transition-all">
-                <h3 class="text-[22px] font-bold my-2 border-l-4 border-main-50 pl-3 group-hover/title:text-main-100 transition-all duration-200 flex items-center mb-5 bottom-0">
-                    <ClientIcon :icon="props.icon" class="text-main-100 mr-2" /> {{ props.title }}
+        <section :class="[
+            'translate-y-[calc(100%-50px)] group-hover/title:translate-y-0 group-focus/title:translate-y-0 absolute bottom-0 p-0 transition-all group-hover/title:backdrop-blur-[3px] group-focus/title:backdrop-blur-[3px]! group-hover/title:bg-white/80! group-focus/title:bg-white/80! dark:group-hover/title:bg-bod/80! dark:group-focus/title:bg-bod/80!',
+            
+        ]">
+            <div class="mx-4 z-10 transition-all">
+                <h3 class="text-[25px] font-bold my-2 border-l-4 border-main-50 pl-3 light:group-hover/title:text-main-400 dark:group-hover/title:text-main-100 transition-all duration-200 flex items-center mb-5 bottom-0 light:text-tod light:group-focus/title:text-tol">
+                    <ClientIcon :icon="props.icon" class="text-main-100 light:group-hover/title:text-main-400 light:group-focus/title:text-main-400 mr-2" /> {{ props.title }}
                 </h3>
                 <p 
                     class="dark:text-tod/80 pb-4"
